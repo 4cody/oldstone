f77d4ad654d4a534c4ba414a86a347afaf902036
Cody Shaner <cshaner04@gmail.com>
2020-10-05 19:08:19 -0500

    init commit

---
+++ assets/js/nav.js
@@ -0,0 +1,14 @@
+const navIcon = document.querySelector('#nav-icon');
+const navList = document.querySelector('#nav-list');
+
+let navIsOpen = false;
+
+const handleNavClick = (e) => {
+  !navIsOpen
+    ? (navList.style.display = 'block')
+    : (navList.style.display = 'none');
+
+  navIsOpen = !navIsOpen;
+};
+
+navIcon.addEventListener('click', handleNavClick);
